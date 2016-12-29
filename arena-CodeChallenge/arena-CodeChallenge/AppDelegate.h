90884ba1a684d738a9a40346c1f78308b24fdf16
Maila  Manzur <mailamanzur@Mailas-MBP.home>
2016-12-29 15:11:08 -0200

    Initial commit

---
+++ arena-CodeChallenge/arena-CodeChallenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  arena-CodeChallenge
+//
+//  Created by Maila  Manzur on 29/12/16.
+//  Copyright © 2016 Maila  Manzur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
